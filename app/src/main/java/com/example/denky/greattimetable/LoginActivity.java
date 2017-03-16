b984a54b7412ffc5edb24f2859cb2c5a71e1ec25
정위대 <denkybrain@naver.com>
2017-03-16 15:58:06 +0900

    base

---
+++ app/src/main/java/com/example/denky/greattimetable/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.denky.greattimetable;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button enter_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        enter_btn = (Button)findViewById(R.id.enter_btn);
+
+
+
+        enter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/denky/greattimetable/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.denky.greattimetable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by denky on 2017-03-05.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
