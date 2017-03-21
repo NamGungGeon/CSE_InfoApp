745a23c836ba65d8ad952a0e179f3076366f660a
정위대 <denkybrain@naver.com>
2017-03-21 16:54:29 +0900

    Did back-end work. made php sources and table in the database. *every categories are linked to the database & table. *phpDown class is constructed.

--- app/src/main/java/com/example/denky/greattimetable/MainActivity.java
+++ app/src/main/java/com/example/denky/greattimetable/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -58,95 +57,19 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<ListItem> listitem = new ArrayList<ListItem>();
     ListItem Item;
 
-    String myJSON;
+    ListView first_listview;
+    ListView second_listview;
+    ListView third_listview;
+    ListView fourth_listview;
     ListView fifth_listview;
-    private static final String TAG_RESULTS="result";
-    private static final String TITLE = "title";
-    private static final String URL = "url";
-    private static final String UPDATER ="updater";
-
-    JSONArray peoples = null;
-
-    ArrayList<HashMap<String, String>> personList;
-
-    protected void showList(){
-        try {
-            JSONObject jsonObj = new JSONObject(myJSON);
-            peoples = jsonObj.getJSONArray(TAG_RESULTS);
-
-            for(int i=0;i<peoples.length();i++){
-                JSONObject c = peoples.getJSONObject(i);
-                String temp_id = c.getString(TITLE);
-                String temp_name = c.getString(URL);
-                String temp_updater = c.getString(UPDATER);
-                HashMap<String,String> persons = new HashMap<String,String>();
-                persons.put(TITLE,temp_id);
-                persons.put(URL,temp_name);
-                persons.put(UPDATER,temp_updater);
-                personList.add(persons);
-
-            }
-
-            ListAdapter adapter = new SimpleAdapter(
-                    MainActivity.this, personList, R.layout.list_item,
-                    new String[]{TITLE,URL,UPDATER},
-                    new int[]{R.id.list_text, R.id.list_context}
-            );
-            fifth_listview.setAdapter(adapter);
-
-
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    }
-    public void getData(String url){
-        class GetDataJSON extends AsyncTask<String, Void, String>{
-
-            @Override
-            protected String doInBackground(String... params) {
-
-                String uri = params[0];
-
-                BufferedReader bufferedReader = null;
-                try {
-                    URL url = new URL(uri);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    StringBuilder sb = new StringBuilder();
-
-                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-                    String json;
-                    while((json = bufferedReader.readLine())!= null){
-                        sb.append(json+"\n");
-                    }
-                    return sb.toString().trim();
-
-                }catch(Exception e){
-                    return null;
-                }
-            }
-
-            @Override
-            protected void onPostExecute(String result){
-                myJSON=result;
-                showList();
-            }
-        }
-        GetDataJSON g = new GetDataJSON();
-        g.execute(url);
-    }
-
-
     protected void onCreate(Bundle savedInstanceState) {
         // ScrollView main_scrollview = (ScrollView) findViewById(R.id.main_scrollView);//스크롤뷰 선언
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView first_listview = (ListView)findViewById(R.id.first_listview);
-        ListView second_listview = (ListView)findViewById(R.id.second_listview);
-        ListView third_listview = (ListView)findViewById(R.id.third_listview);
-        ListView fourth_listview = (ListView)findViewById(R.id.fourth_listview);
+        first_listview = (ListView)findViewById(R.id.first_listview);
+        second_listview = (ListView)findViewById(R.id.second_listview);
+        third_listview = (ListView)findViewById(R.id.third_listview);
+        fourth_listview = (ListView)findViewById(R.id.fourth_listview);
         fifth_listview = (ListView)findViewById(R.id.fifth_listview);
         Button contest_btn = (Button)findViewById(R.id.contest_btn);
         pager = (ViewPager) findViewById(R.id.pager);
@@ -161,70 +84,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /******************포스터 Bitmap 가공******************
-         for (int i = 0; i < maxScroll; i++) {
-         Img[i] = BitmapFactory.decodeResource(getResources(), R.drawable.poster1 + i);
-         Img[i] = cropBitmap(Img[i]);
-         }
-         adapter.getImg(maxScroll, Img); // 가공된 비트맵 전달
-         /******************포스터 Bitmap 가공******************/
-        //ViewPager에 Adapter 설정
-        //카테고리 소스 : 1
-        //카테고리 소스 : 2
-        ArrayList<Listview> category1_data=new ArrayList<>();
-        Listview category1_l1=new Listview("국가장학재단","클릭하세용", "국가장학금1");
-        Listview category1_l2=new Listview("국가장학재단","클릭하세용2", "국가장학금2");
-        Listview category1_l3=new Listview("국가장학재단","클릭하세용3", "국가우수장학금(이공계)");
 
-        category1_data.add(category1_l1);
-        category1_data.add(category1_l2);
-        category1_data.add(category1_l3);
-        ListviewAdapter category1_adapter=new ListviewAdapter(this,R.layout.list_item,category1_data , 2);
-        first_listview.setAdapter(category1_adapter);
-        //카테고리 소스 : 2
-        //카테고리 소스 : 2
-        ArrayList<Listview> category2_data=new ArrayList<>();
-        Listview category2_l1=new Listview("건대파워코더","", "닭그네 탄핵됌ㅅㄱ링");
-        Listview category2_l2=new Listview("코딩장인","클릭하세용2", "하루500줄코딩미만잡");
-       // Listview category2_l3=new Listview("국가장학재단","클릭하세용3", "국가우수장학금(이공계)");
+        phpDown php1 = new phpDown(1, first_listview, MainActivity.this);
+        php1.getData("http://denkybrain.cafe24.com/cse/scholarship.php");
 
-        category2_data.add(category2_l1);
-        category2_data.add(category2_l2);
-        //category2_data.add(category2_l3);
-        ListviewAdapter category2_adapter=new ListviewAdapter(this,R.layout.list_item,category2_data , 2);
-        second_listview.setAdapter(category2_adapter);
-        //카테고리 소스 : 2
+        phpDown php2 = new phpDown(2, second_listview, MainActivity.this);
+        php2.getData("http://denkybrain.cafe24.com/cse/community.php");
 
-        //카테고리 소스 : 3
-        ArrayList<Listview> category3_data=new ArrayList<>();
-        Listview category3_l1=new Listview("국가장학재단","클릭하세용", "국가장학금1");
-        Listview category3_l2=new Listview("국가장학재단","클릭하세용2", "국가장학금2");
-        Listview category3_l3=new Listview("국가장학재단","클릭하세용3", "국가우수장학금(이공계)");
-
-        category3_data.add(category3_l1);
-        category3_data.add(category3_l2);
-        category3_data.add(category3_l3);
-        ListviewAdapter category3_adapter=new ListviewAdapter(this,R.layout.list_item,category3_data, 3);
-        third_listview.setAdapter(category3_adapter);
-        //카테고리 소스 : 3
-
-        //카테고리 소스 : 4
-        ArrayList<Listview> category4_data=new ArrayList<>();
-        Listview category4_l1=new Listview("국가장학재단","클릭하세용", "국가장학금1");
-        Listview category4_l2=new Listview("국가장학재단","클릭하세용2", "국가장학금2");
-        Listview category4_l3=new Listview("국가장학재단","클릭하세용3", "국가우수장학금(이공계)");
-
-        category4_data.add(category4_l1);
-        category4_data.add(category4_l2);
-        category4_data.add(category4_l3);
-        ListviewAdapter category4_adapter=new ListviewAdapter(this,R.layout.list_item,category4_data, 4);
-        fourth_listview.setAdapter(category4_adapter);
-        //카테고리 소스 : 4
-        personList = new ArrayList<HashMap<String,String>>();
+        phpDown php3 = new phpDown(3, third_listview, MainActivity.this);
+        php3.getData("http://denkybrain.cafe24.com/cse/team.php");
+        phpDown php4 = new phpDown(4, fourth_listview, MainActivity.this);
+        php4.getData("http://denkybrain.cafe24.com/cse/usedmarket.php");
         //카테고리 소스 : 5  - 추천 사이트 ----- //
-        getData("http://denkybrain.cafe24.com/cse/recommend_site2.php");
-
-
+        phpDown php5 = new phpDown(5, fifth_listview, MainActivity.this);
+        php5.getData("http://denkybrain.cafe24.com/cse/recommend_site2.php");
         //카테고리 소스 : 5
 
         pager.setAdapter(adapter);
---
+++ app/src/main/java/com/example/denky/greattimetable/phpDown.java
@@ -0,0 +1,154 @@
+package com.example.denky.greattimetable;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by denky on 2017-03-21.
+ */
+
+public class phpDown {
+    int type ;
+    String myJSON;
+    ListView listview;
+    Activity activity;
+    private static final String TAG_RESULTS="result";
+    private static final String TITLE = "title";
+    private static final String URL = "url";
+    private static final String UPDATER ="updater";
+    private static final String CONTEXT ="context";
+    ArrayList<HashMap<String, String>> personList;
+
+
+    JSONArray recommend_sites = null;
+    public phpDown (int type, ListView listview, Activity activity) {
+        this.type = type; // 순서대로 1카테고리 = type 1, 5카테고리 = type 5
+        this.listview = listview;
+        this.activity = activity;
+        personList = new ArrayList<HashMap<String,String>>();
+
+       // Log.d("php_result", "type :"+this.type + "\nlistview : "+this.listview +"\nactivity : "+this.activity);
+    }
+
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            recommend_sites = jsonObj.getJSONArray(TAG_RESULTS);
+            for(int i = 0; i< recommend_sites.length(); i++){
+                JSONObject c = recommend_sites.getJSONObject(i);
+                String temp_id = c.getString(TITLE);
+                String temp_url = c.getString(URL);
+                String temp_updater = c.getString(UPDATER);
+                String temp_context = c.getString(CONTEXT);
+                HashMap<String,String> persons = new HashMap<String,String>();
+                persons.put(TITLE,temp_id);
+                persons.put(URL,temp_url);
+                persons.put(UPDATER,temp_updater);
+                persons.put(CONTEXT, temp_context);
+                personList.add(persons);
+            }
+           // Log.d("php_result", type+ ","+TITLE +","+URL);
+            switch (type){
+                case 1 :  ListAdapter adapter = new SimpleAdapter(
+                        activity, personList, R.layout.list_item,
+                        new String[]{TITLE,CONTEXT,URL},
+                        new int[]{R.id.list_text, R.id.list_context}
+                );
+                    listview.setAdapter(adapter);
+                    break;
+                case 2 : ListAdapter adapter2 = new SimpleAdapter( //default_adapter
+                        activity, personList, R.layout.list_item,
+                        new String[]{UPDATER, TITLE, URL},
+                        new int[]{R.id.list_text, R.id.list_context}
+                );
+                    listview.setAdapter(adapter2);
+                    break;
+                case 3 : ListAdapter adapter3 = new SimpleAdapter( //default_adapter
+                        activity, personList, R.layout.list_item,
+                        new String[]{CONTEXT, TITLE},
+                        new int[]{R.id.list_text, R.id.list_context}
+                );
+                    listview.setAdapter(adapter3);
+                    break;
+                case 4 : ListAdapter adapter4 = new SimpleAdapter( //default_adapter
+                        activity, personList, R.layout.list_item,
+                        new String[]{UPDATER, TITLE},
+                        new int[]{R.id.list_text, R.id.list_context}
+                );
+                    listview.setAdapter(adapter4);
+                    break;
+
+                case 5 :
+                        ListAdapter adapter5 = new SimpleAdapter( //default_adapter
+                        activity, personList, R.layout.list_item,
+                        new String[]{TITLE, URL},
+                        new int[]{R.id.list_text, R.id.list_context}
+                );
+                    listview.setAdapter(adapter5);
+                    break;
+
+                default :
+                    break;
+
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public void getData(String url){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine())!= null){
+                        sb.append(json+"\n");
+                    }
+                    return sb.toString().trim();
+
+                }catch(Exception e){
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+             //  Log.d("php_result", "type : " +type+ result);
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+}
